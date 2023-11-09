9cd12c2dbe5eaec76c3073382672c15098f1eaa3
Zaid141 <zaidshahid141414@gmail.com>
2023-11-09 18:51:43 -0500

    view directory

--- app.mjs
+++ app.mjs
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // configure templating to hbs
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 // body parser (req.body)
 app.use(express.urlencoded({ extended: false }));
